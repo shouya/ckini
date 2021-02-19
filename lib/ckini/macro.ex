aa2eb59b0567786c2234ae8c7e66e36ad8710da9
Shou Ya <shouya@users.noreply.github.com>
2021-02-19 19:04:43 +0800

    basic macro interfaces

--- lib/ckini/macro.ex
+++ lib/ckini/macro.ex
@@ -5,84 +5,90 @@ defmodule Ckini.Macro do
   Exports run/1, run/2, fresh/1, conde/1.
   """
 
-  alias Ckini.{Stream, Var, Term}
+  alias Ckini.{Stream, Context}
 
-  defmacro run(n, {:fn, _, [{:->, _, [[var], body]}]})
-           when is_integer(n) and n >= 0 do
-    [logic_var] = to_logic_vars([var])
-    var_assignments = to_var_assignments([var], [logic_var])
-
-    goals =
-      case body do
-        {:__block__, _, gs} -> Enum.map(gs, &Macro.expand(&1, __CALLER__))
-        g -> [Macro.expand(g, __CALLER__)]
-      end
-
-    quote location: :keep do
-      unquote_splicing(var_assignments)
-      goals = unquote(goals)
-
-      Subst.new()
-      |> Stream.singleton()
-      |> Stream.bind_goals(goals)
-      |> Stream.map(fn subst ->
-        Term.reify(unquote(Macro.escape(logic_var)), subst)
-      end)
-      |> Stream.take(unquote(n))
+  defmacro fresh(vars, do: goals) do
+    quote do
+      unquote(generate_vars(vars))
+      all(do: unquote(goals))
     end
   end
 
-  defmacro run({:fn, _, [{:->, _, [[var], body]}]}) do
-    [logic_var] = to_logic_vars([var])
-    var_assignments = to_var_assignments([var], [logic_var])
+  defmacro run(n, vars, do: goals) do
+    quote do
+      unquote(generate_vars(vars))
+      goal = all(do: unquote(goals))
 
-    goals =
-      case body do
-        {:__block__, _, gs} -> Enum.map(gs, &Macro.expand(&1, __CALLER__))
-        g -> [Macro.expand(g, __CALLER__)]
-      end
-
-    quote location: :keep do
-      unquote_splicing(var_assignments)
-      goals = unquote(goals)
-
-      Subst.new()
-      |> Stream.singleton()
-      |> Stream.bind_goals(goals)
-      |> Stream.map(fn subst ->
-        Term.reify(unquote(Macro.escape(logic_var)), subst)
-      end)
+      goal
+      |> apply([Stream.new(Context.new())])
+      |> Stream.map(&Term.reify(unquote(vars), &1))
+      |> Stream.take(unquote(n))
       |> Stream.to_list()
     end
   end
 
-  defmacro fresh({:fn, _, [{:->, _, [vars, body]}]}) do
-    logic_vars = to_logic_vars(vars)
-    var_assignments = to_var_assignments(vars, logic_vars)
+  defmacro all(do: goals) do
+    bind_goals(goals)
+  end
 
-    goals =
-      case body do
-        {:__block__, _, gs} -> Enum.map(gs, &Macro.expand(&1, __CALLER__))
-        g -> [Macro.expand(g, __CALLER__)]
+  defmacro conde(do: cases) do
+    clauses =
+      for {:->, _, [vars, clause]} <- cases do
+        quote do
+          fn -> fresh(unquote(vars), do: unquote(clause)).(ctx) end
+        end
       end
 
-    quote location: :keep do
-      fn s ->
-        unquote_splicing(var_assignments)
-        Stream.bind_goals(Stream.singleton(s), unquote(goals))
+    quote do
+      fn ctx ->
+        Stream.mplus_many(unquote(clauses))
       end
     end
   end
 
-  defp to_logic_vars(vars) do
-    for {var, _, _} <- vars do
-      Var.new(var)
+  defp bind_goals({:__block__, _, []}) do
+    quote do: &Stream.singleton/1
+  end
+
+  defp bind_goals({:__block__, metadata, [g | gs]}) do
+    quote do
+      fn ctx ->
+        ctxs = unquote(g).(ctx)
+        goal = unquote(bind_goals({:__block__, metadata, gs}))
+
+        ctxs
+        |> Stream.map(fn ctx -> goal.(ctx) end)
+        |> Stream.mplus_many()
+      end
     end
   end
 
-  defp to_var_assignments(quoted_vars, logic_vars) do
-    for {qv, lv} <- Enum.zip(quoted_vars, logic_vars) do
-      quote do: unquote(qv) = unquote(Macro.escape(lv))
+  defp bind_goals(single) do
+    bind_goals({:__block__, [], [single]})
+  end
+
+  defp generate_vars({:{}, _, vars}) do
+    generate_vars(vars)
+  end
+
+  defp generate_vars(vars) when is_list(vars) do
+    quote do
+      (unquote_splicing(
+         for {name, _, _} = var <- vars do
+           quote do: unquote(var) = Ckini.Var.new(unquote(name))
+         end
+       ))
+    end
+  end
+
+  defp generate_vars({:_, _, _}) do
+    quote do
+    end
+  end
+
+  defp generate_vars({name, _, _} = var) do
+    quote do
+      unquote(var) = Ckini.Var.new(unquote(name))
     end
   end
 end
