0c8a1b46d8cf2b884f062672ee62173178fab1cb
Shou Ya <shouya@users.noreply.github.com>
2021-02-24 21:02:55 +0800

    add ex_doc dependencies for generating documentation

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule Ckini.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp package do
