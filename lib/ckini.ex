df3d2abc1b6c04b08d2012f4b340ab0c28bb7e59
Shou Ya <shouya@users.noreply.github.com>
2021-02-24 21:12:46 +0800

    add simple moduledoc for Ckini

--- lib/ckini.ex
+++ lib/ckini.ex
@@ -1,4 +1,8 @@
 defmodule Ckini do
+  @moduledoc """
+  Imports primitives from Ckini.Goals and Ckini.Macro.
+  Check out the documentation for these two modules for details.
+  """
   defmacro __using__(opts \\ []) do
     style =
       case Keyword.get(opts, :style, :macro) do
