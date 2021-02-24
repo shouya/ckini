0cd316da76ef89b4ca9d29d5e4e9287f471dab91
Shou Ya <shouya@users.noreply.github.com>
2021-02-24 20:52:05 +0800

    update package info

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Ckini.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -20,9 +21,15 @@ defmodule Ckini.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      name: "ckini",
+      files: ~w(lib mix.exs README LICENSE),
+      licenses: ["GPL-2.0"],
+      links: %{"GitHub" => "https://github.com/shouya/ckini"}
     ]
   end
 end
